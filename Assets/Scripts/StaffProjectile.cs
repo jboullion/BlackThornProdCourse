5ef5de4cb7a58e33a0b058996a70974f78c664c7
jboullion <jboullion83@gmail.com>
2019-07-29 00:13:28 -0500

    Player and Camera Movement updated. Arena laid out.

---
+++ Assets/Scripts/StaffProjectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaffProjectile : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+    [SerializeField] float lifeTime;
+
+    [SerializeField] GameObject explosion;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    private void DestroyProjectile()
+    {
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
